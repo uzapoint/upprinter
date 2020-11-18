14f3766691e2b4dea9b37404837395a1331ffff6
thukuwanjiku <thukuwanjikum@gmail.com>
2020-11-18 13:58:32 +0300

    - rename 'Customer Pickup' to 'Pickup Order' for a pickup order

--- application/libraries/Printer_lib.php
+++ application/libraries/Printer_lib.php
@@ -90,7 +90,7 @@ class Printer_lib
                 $printer->text("------------------------------------------------\n");
                 $printer->feed();
                 $printer->text("DELIVERY DETAILS\n\n");
-                $printer->text('Delivery: ' . ($request['order_delivery_method'] == 'pickup' ? 'Customer Pickup' : 'Delivery Order') . "\n");
+                $printer->text('Delivery: ' . ($request['order_delivery_method'] == 'pickup' ? 'Pickup Order' : 'Delivery Order') . "\n");
                 if(!empty($request['order_delivery_location'])) $printer->text('Location: ' . $request['order_delivery_location'] . "\n");
                 if(!empty($request['order_delivery_address'])) $printer->text('Address: ' . $request['order_delivery_address'] . "\n");
                 if(!empty($request['order_delivery_cost'])) $printer->text('Delivery Cost: ' . $request['order_delivery_cost'] . "\n");
