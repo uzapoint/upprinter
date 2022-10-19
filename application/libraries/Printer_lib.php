eb7fadae1dba9200499cd4fbe214e1b92e977297
jacodhis <odhiambojack536@gmail.com>
2022-10-19 14:50:49 +0300

    test code

--- application/libraries/Printer_lib.php
+++ application/libraries/Printer_lib.php
@@ -59,14 +59,14 @@ class Printer_lib
 
         //add items heading
         // $header = sprintf("%-28s %-5s %-9s", "Item", "Qty", "Total");
-        $printer->text('Item'.'   '.'Qty'.'   '.'Total');
+        $printer->text('Item'.'        '.'Qty'.'   '.'Total');
         // $printer->text($header );
         $printer->text("-------------------------------------\n");
 
         //add the items
         foreach ($request['items'] as $item) {
             //$myItem = sprintf("%-28s %-5s %-9s", substr($item['item_name_only'], 0, 27), $item['qty'], number_format((float)$item['total'], 2));
-            $myItem = substr($item['item_name_only'], 0, 27).'   '.$item['qty'].'   '.number_format((float)$item['total'], 2);
+            $myItem = substr($item['item_name_only'], 0, 27).'        '.$item['qty'].'   '.number_format((float)$item['total'], 2);
             $myItem = str_replace(PHP_EOL,'',$myItem);
             $printer->text($myItem);
             $printer->feed();
