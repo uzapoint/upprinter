f5e65b01d492a1a3e56c4fd92793526d7fd8012e
jacodhis <odhiambojack536@gmail.com>
2022-10-19 13:43:40 +0300

    testing imported code

--- application/libraries/Printer_lib.php
+++ application/libraries/Printer_lib.php
@@ -65,9 +65,8 @@ class Printer_lib
         //add the items
         foreach ($request['items'] as $item) {
             $myItem = sprintf("%-28s %-5s %-9s", substr($item['item_name_only'], 0, 27), $item['qty'], number_format((float)$item['total'], 2));
-            $printer->text($myItem. "\n");
-            // echo "\n";
-            echo trim(" ");
+            $printer->text($myItem);
+            $connector->write(self::ESC."d".chr(1));
         }
         $printer->text("-------------------------------------\n");
 
