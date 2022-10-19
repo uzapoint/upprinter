06e58759bb9cfe49636c46b15207512ba97575b8
jacodhis <odhiambojack536@gmail.com>
2022-10-19 16:31:16 +0300

    testing imported code

--- application/libraries/Printer_lib.php
+++ application/libraries/Printer_lib.php
@@ -69,10 +69,9 @@ class Printer_lib
         foreach ($request['items'] as $item) {
             // $myItem = sprintf("%-28s %-5s %-9s", substr($item['item_name_only'], 0, 27), $item['qty'], number_format((float)$item['total'], 2));
             // $myItem = substr($item['item_name_only'], 0, 27).'          '.$item['qty'].'   '.number_format((float)$item['total'], 2);
-            // $numbers = strlen(sprintf("    %-3s %-8s",$item['qty'], number_format((float)$item['total'], 2)));
-            // $myItem = sprintf("%-29s %-3s %-8s", substr($item['item_name_only'], 0, 42-($numbers)), $item['qty'], number_format((float)$item['total'], 2));
-            $numbers = (sprintf("%3s  %s",$item['qty'], number_format((float)$item['total'], 2)));
-            $myItem = sprintf("%s   %s  %s", substr($item['item_name_only'], 0, (42-strlen($numbers))), $item['qty'], number_format((float)$item['total'], 2));
+            $numbers = strlen(sprintf("    %-3s %-8s",$item['qty'], number_format((float)$item['total'], 2)));
+            $myItem = sprintf("%-27s %-3s %-8s", substr($item['item_name_only'], 0, 42-($numbers)), $item['qty'], number_format((float)$item['total'], 2));
+
 
             // $myItem = sprintf("%-29s %-3s %-6s", substr($item['item_name_only'], 0, 27), $item['qty'], number_format((float)$item['total'], 2));
             $myItem = str_replace(PHP_EOL,'',$myItem);
