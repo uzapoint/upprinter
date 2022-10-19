cf76dd2d009ae8c5fba33e07655919e9e2394d47
jacodhis <odhiambojack536@gmail.com>
2022-10-19 13:33:20 +0300

    added an echo for br

--- application/libraries/Printer_lib.php
+++ application/libraries/Printer_lib.php
@@ -65,7 +65,7 @@ class Printer_lib
         //add the items
         foreach ($request['items'] as $item) {
             $myItem = sprintf("%-28s %-5s %-9s", substr($item['item_name_only'], 0, 27), $item['qty'], number_format((float)$item['total'], 2));
-            $printer->text($myItem,"\n");
+            $printer->text($myItem. "\n");
             // echo "\n";
             echo trim(" ");
         }
