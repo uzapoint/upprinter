c98aa8bb8dade26d79b067ec4708e6ff7f3f0bd8
caleb98kymez <caleb98kymez@gmail.com>
2023-07-18 08:40:36 +0300

    Fixed bug in end shift report

--- application/libraries/Printer_lib.php
+++ application/libraries/Printer_lib.php
@@ -1255,7 +1255,7 @@ class Printer_lib
             $printer->feed();
 
 
-            if ($request['has_breakdown']){
+            if (!$request['has_breakdown']){
                 foreach ($request['collections'] as $index => $collection) {
                     $myItem = sprintf("%-18s %-8s %-8s %-8s", $collection, $request['actual'][$index], $request['expected'][$index], $request['variance'][$index]);
                     if ($index === (sizeof($request['collections']) - 1)) {
