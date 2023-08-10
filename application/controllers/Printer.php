5b32a3f4adafae504521397a12aec5996107aa8a
caleb98kymez <caleb98kymez@gmail.com>
2023-08-10 11:42:24 +0300

    Changes to fix CORS errors.

--- application/controllers/Printer.php
+++ application/controllers/Printer.php
@@ -4,13 +4,59 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Printer extends CI_Controller
 {
 
+    public function __construct()
+    {
+        parent::__construct();
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method == "OPTIONS") {
+            die();
+        }
+    }
+
     public function proforma()
     {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method == "OPTIONS") {
+            die();
+        }
+
         $this->load->library('printer_lib');
-
         $request = $this->input->post();
-
         $this->printer_lib->bill($request);
+
+        set_status_header(200);
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+        header('Content-Type: application/json;charset=UTF-8');
+        exit(json_encode([
+            "message" => "Successfully printed receipt"
+        ]));
+    }
+
+    public function receipt()
+    {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method == "OPTIONS") {
+            die();
+        }
+
+        $data = json_decode($this->input->raw_input_stream, true);
+        $this->load->library('printer_lib');
+        $this->printer_lib->print_receipt($data);
+        set_status_header($http_code);
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+        header('Content-Type: application/json;charset=UTF-8');
+        exit(json_encode($output));
     }
 
     public function captain()
