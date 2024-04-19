1be491697264d387c3cf3c4d559be7b53a536aaf
thukuwanjiku <thukuwanjikum@gmail.com>
2024-04-19 09:36:58 +0300

    - where digitax etims details are provided, print serial number, signature and etims url qr code on receipt
    Signed-off-by: thukuwanjiku <thukuwanjikum@gmail.com>

--- application/libraries/Printer_lib.php
+++ application/libraries/Printer_lib.php
@@ -542,6 +542,17 @@ class Printer_lib
                 $printer->selectPrintMode();
                 $printer->qrCode($signedInvoiceDetails['qr_code_url'], Printer::QR_ECLEVEL_L, 6);
             }
+            /*
+             * CHECK IF DIGITAX ETIMS DETAILS ARE PROVIDED, PRINT QR Code
+             * */
+            if (!empty($request['digitax_etims_details'])) {
+                $digitaxEtimsDetails = $request['digitax_etims_details'];
+                $printer->feed(2);
+                $printer->text("Serial No: " . $digitaxEtimsDetails['serial_number'] . "\n");
+                $printer->text("Signature: " . $digitaxEtimsDetails['signature'] . "\n");
+                $printer->selectPrintMode();
+                $printer->qrCode($digitaxEtimsDetails['etims_url'], Printer::QR_ECLEVEL_L, 6);
+            }
 
             $printer->setJustification();
             $printer->feed(5);
