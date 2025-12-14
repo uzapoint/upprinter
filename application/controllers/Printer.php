c0b9425f6a0d0fbeb1e186cffbd8893a7457ffc7
morexex <ynanaromorexex@gmail.com>
2025-12-14 10:32:21 +0300

    fix cors error for timsEtrTypeB

--- application/controllers/Printer.php
+++ application/controllers/Printer.php
@@ -98,6 +98,14 @@ class Printer extends CI_Controller
 
     public function timsEtrTypeB()
     {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Methods: POST, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type");
+
+        // Handle the Preflight check
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            exit(0);
+        }
         $this->load->library('printer_lib');
 
         $request = $this->input->post();
